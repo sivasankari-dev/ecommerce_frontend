67a015e7e22f1ed1f345f092f227a26a5d10db5d
sivasankari-dev <sivasankari.devofficial@gmail.com>
2026-03-11 19:29:02 +0530

    Adding production url to env variable
    Adding render backend url to frontend

--- src/api.js
+++ src/api.js
@@ -1,9 +1,27 @@
-import axios from "axios"
+// import axios from "axios"
 
-export const BASE_URL = "http://127.0.0.1:8000/"
+// export const BASE_URL = "http://127.0.0.1:8000/"
+
+// const api = axios.create({
+//     baseURL: "http://127.0.0.1:8000/"
+// })
+
+// export default api
+
+import axios from "axios";
+
+// Fallback to localhost if the environment variable isn't set
+export const BASE_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000/";
 
 const api = axios.create({
-    baseURL: "http://127.0.0.1:8000/"
-})
+    baseURL: BASE_URL,
+    // Recommended: add headers if you're sending JSON
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
 
-export default api
+// If you need to handle Django CSRF tokens or Cookies later:
+// api.defaults.withCredentials = true;
+
+export default api;
